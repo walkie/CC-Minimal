558bf3b52568e0610a1bcdd3362ebbc8128bb945
Eric Walkingshaw <eric@walkingshaw.net>
2014-01-29 16:41:04 +0100

    Update dependencies and test suite.

--- test/all-tests.hs
+++ test/all-tests.hs
@@ -5,9 +5,11 @@ import Test.Tasty
 
 import CC.LanguageTest
 import CC.ObjectTest
+import CC.ParserTest
 
 main = defaultMain $
   testGroup "CC-Minimal" [
     CC.LanguageTest.tests,
-    CC.ObjectTest.tests
+    CC.ObjectTest.tests,
+    CC.ParserTest.tests
   ]
